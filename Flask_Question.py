78ea1e7e9aa4f23558c7d61ac273eb54d7fe118e
suwenkun1126 <suwenkun1126@126.com>
2018-04-11 20:10:12 +0800

    创建Question模型并完成提问视图函数

--- Flask_Question.py
+++ Flask_Question.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,url_for,request,redirect,session
 import config
 from exts import db
-from models import User
+from models import User,Question
 from decorators import login_required
 
 app = Flask(__name__)
@@ -59,7 +59,15 @@ def question():
     if request.method == 'GET':
         return render_template('question.html')
     else:
-        pass
+        title = request.form.get('title')
+        content = request.form.get('content')
+        question = Question(title=title,content=content)
+        user_id = session.get('user_id')
+        user = User.query.filter(User.id == user_id).first()
+        question.author = user
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('index'))
 
 @app.context_processor
 def my_context_processor():
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 from Flask_Question import app
 from exts import db
-from models import User
+from models import User,Question
 
 migrate = Migrate(app,db)
 manager = Manager(app)
